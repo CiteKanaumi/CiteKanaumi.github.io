59b33eee433eda3ddd3a8a9c7173314c9269fd71
Kim Jaehyen <lastofakiha@gmail.com>
2018-10-15 16:45:57 +0900

    a

--- post/past.py
+++ post/past.py
@@ -11,6 +11,7 @@ for x in os.listdir(path):
         files.append(x)
 
 data_list = pd.DataFrame(columns=['Fighter\'s ID'])
+data_list2 = pd.DataFrame(columns=['Fighter\'s ID'])
 
 for (file0, i) in zip(files, range(len(files))):
     test_data = pd.read_csv('./sfv/'+file0+'.csv', dtype='object')
@@ -18,5 +19,14 @@ for (file0, i) in zip(files, range(len(files))):
     test_data = test_data.rename(columns={'LP': file0})
     data_list = pd.merge(data_list, test_data, right_on='Fighter\'s ID', left_on='Fighter\'s ID', how='outer')
 
+    test_data2 = pd.read_csv('./sfv/'+file0+'.csv', dtype='object')
+    test_data2 = test_data2[['Fighter\'s ID', 'Matches.1']]
+    test_data2 = test_data2.rename(columns={'Matches.1': file0})
+    data_list2 = pd.merge(data_list2, test_data2, right_on='Fighter\'s ID', left_on='Fighter\'s ID', how='outer')
+    
+
 data_list = data_list.set_index('Fighter\'s ID')
 data_list.to_csv('./sfv/merge/merge_list.csv')
+
+data_list2 = data_list2.set_index('Fighter\'s ID')
+data_list2.to_csv('./sfv/merge/merge_list2.csv')
