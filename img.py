1f442bf8c7a0c6d6d180dcfef60559d183eddb13
LOAcite <lastofakiha@gmail.com>
2020-05-25 10:11:12 +0900

    a

---
+++ img.py
@@ -0,0 +1,85 @@
+import glob
+import re
+
+files = glob.glob("./img/vrc/*.jpg")
+
+
+h_f = """
+
+---
+layout: default
+---
+<div class="well">
+    {% if page.subtitle %}
+    <h4 class="home-subtitle">{{ page.subtitle }}</h4>
+    {% endif %}
+    <div>
+    	{{ content }}
+        
+        
+        
+  <head>
+    <link rel="stylesheet" href="../css/grid-gallery.min.css">
+  </head>
+
+  <body>
+
+    <div class="content">
+      <div class="gg-container">
+        <div class="gg-box dark" id="square">
+
+
+
+"""
+
+h_b = """
+
+        </div>
+      </div>
+    </div>
+
+    <script src="../js/grid-gallery.min.js"></script>
+    <script>
+    gridGallery({
+      selector: "#square",
+      layout: "square"
+    });
+    </script>
+
+  </body>
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </div>
+</div>
+
+"""
+
+
+
+
+newfiles2 = ['<img src=".'+file+'">' for file in files]
+
+newfiles = [re.sub(r'\\', '/', file) for file in newfiles2]
+
+
+with open('./_includes/vrchat.html', 'w') as f:
+
+
+	f.write(h_f)
+
+
+	for d in newfiles:
+		f.write("%s\n" % d)
+
+
+	f.write(h_b)
